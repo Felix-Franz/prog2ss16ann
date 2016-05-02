e1ec2bdc4d5fe5bd4d56f5e6684b7623160d3242
Felix-Franz <felix-franz@t-online.de>
2016-05-02 17:09:31 +0200

    Update NeuronalNetwork.java

--- src/ann/NeuronalNetwork.java
+++ src/ann/NeuronalNetwork.java
@@ -1,7 +1,7 @@
 package ann;
 
 /**
- * Created by patrickmennig on 02.05.16.
+ * Created by patrickmennig on 02.05.16., edited by Felix Franz on 02.05.16
  */
 public class NeuronalNetwork {
 
@@ -11,19 +11,29 @@ public class NeuronalNetwork {
     // layer anlegen + "verbinden"
     // layer speichern
     // NeuronLayer[]
-    public NeuronalNetwork(){
+    public NeuronalNetwork(int numLayers, double[][][] weights){
+    	layers = new NeuronLayer[numLayers];
+    	
+    	for (int i = 0; i<numLayers; i++){
+    		layers[i] = new NeuronLayer(weights[i].length, weights[i]);
+    	}
+    	
         // wie viele Schichten?
         // wie viele Neuronen pro Schicht?
         // welche Gewichte Je neuron je Schicht...
     }
 
-    // vorwärtslauf => eingangssignal annehmen, ausgangsignal erzeugen
+    // vorwÃ¤rtslauf => eingangssignal annehmen, ausgangsignal erzeugen
     // input in die erste Schicht, output der ersten Schicht in die 2. usw...
     public double[] feedForward(double[] input){
-
-        double[] outputFirstLayer = layers[0].feedForward(input);
-        double[] outputSecondLayer = layers[1].feedForward(outputFirstLayer);
-        //...
+    	
+    	double result[];
+    	double previousLayer[];
+    	previousLayer = layers[0].feedForward(input);
+    	for (int i = 1; i < layers.length; i++){
+    		previousLayer = layers[i].feedForward(previousLayer);
+    	}
+    	return previousLayer;
 
     }
 
